ab777db2c39a97b8a721e5a5dde7a37680b8b2da
Aida Begagić <aida.begagic.24@size.ba>
2025-12-30 15:45:07 +0100

    logo i profil stranica dizajn

---
+++ Cinestar-app/Profil.cs
@@ -0,0 +1,15 @@
+namespace Cinestar_app;
+
+public class Profil : ContentPage
+{
+	public Profil()
+	{
+		Content = new VerticalStackLayout
+		{
+			Children = {
+				new Label { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Welcome to .NET MAUI!"
+				}
+			}
+		};
+	}
+}
