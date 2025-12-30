a17bcba2de7089a7c7ebd79a15b0143abc72c2c2
Aida Begagić <aida.begagic.24@size.ba>
2025-12-30 16:11:05 +0100

    funkcionalnost prijava registracija

--- Cinestar-app/Profil.xaml.cs
+++ Cinestar-app/Profil.xaml.cs
@@ -1,9 +1,24 @@
+using Microsoft.Maui.Controls;
+
 namespace Cinestar_app.Pages;
 
 public partial class Profil : ContentPage
 {
-	public Profil()
-	{
-		InitializeComponent();
-	}
+    public Profil()
+    {
+        InitializeComponent();
+    }
+
+    private async void Prijava_Clicked(object sender, EventArgs e)
+    {
+        await DisplayAlert(
+            "Prijava",
+            "Nemate nalog. Molimo vas da se prvo registrujete.",
+            "OK");
+    }
+
+    private void Registracija_Clicked(object sender, EventArgs e)
+    {
+        RegistracijaForma.IsVisible = !RegistracijaForma.IsVisible;
+    }
 }
