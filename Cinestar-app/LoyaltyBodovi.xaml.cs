6d003a769838f6225e047758bdf99112b1768683
Aida Begagić <aida.begagic.24@size.ba>
2026-01-12 18:34:53 +0100

    bodovi dizajn

--- Cinestar-app/LoyaltyBodovi.xaml.cs
+++ Cinestar-app/LoyaltyBodovi.xaml.cs
@@ -6,59 +6,13 @@ namespace Cinestar_app;
 public partial class LoyaltyBodovi : ContentPage
 {
 
-
-    private UserDatabase _db;
-
     public LoyaltyBodovi()
     {
         InitializeComponent();
-        _db = new UserDatabase();
-        NavigationPage.SetHasNavigationBar(this, false);
+
     }
-
-    protected override async void OnAppearing()
+    private async void OnPrijavaButtonClicked(object sender, EventArgs e)
     {
-        base.OnAppearing();
-
-        if (UserSession.IsLoggedIn)
-        {
-            GuestView.IsVisible = false;
-            UserView.IsVisible = true;
-
-            var loyalty = await _db.GetLoyaltyAsync(UserSession.CurrentUser.Email);
-            BodoviLabel.Text = loyalty?.Bodovi.ToString() ?? "0";
-        }
-        else
-        {
-            GuestView.IsVisible = true;
-            UserView.IsVisible = false;
-        }
-    }
-
-    private async void BuyTickets_Clicked(object sender, EventArgs e)
-    {
-        if (!UserSession.IsLoggedIn)
-            return;
-
-        await _db.AddPointsAsync(UserSession.CurrentUser.Email, 10);
-
-        var loyalty = await _db.GetLoyaltyAsync(UserSession.CurrentUser.Email);
-        BodoviLabel.Text = loyalty?.Bodovi.ToString() ?? "0";
-
-        await DisplayAlert("Bravo 🎉", "Dobili ste 10 loyalty bodova!", "OK");
-    }
-
-    private async void ReviewMovie_Clicked(object sender, EventArgs e)
-    {
-        if (!UserSession.IsLoggedIn)
-            return;
-
-        await _db.AddPointsAsync(UserSession.CurrentUser.Email, 5);
-
-        var loyalty = await _db.GetLoyaltyAsync(UserSession.CurrentUser.Email);
-        BodoviLabel.Text = loyalty?.Bodovi.ToString() ?? "0";
-
-        await DisplayAlert("Hvala ⭐", "Dobili ste 5 loyalty bodova!", "OK");
+        await Navigation.PushAsync(new Profil());
     }
 }
-
